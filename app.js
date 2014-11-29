ae4f91c38c998fa0e15ff70560165523b4d96b20
Tyler Christensen <tyler9xp@gmail.com>
2014-11-29 13:38:57 -0700

    A little reorganizing

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var express = require('express'),
 	git = require('git-rev'),
 	_ = require('underscore'),
-	socketio = require('socket.io');
+	socketio = require('socket.io'),
+	path = require('path');
 
 git.short(function(rev) {
 	console.log('Current revision: ' + rev);
 });
+
+global.__paths = {
+	client: path.normalize(path.join(__dirname, './src/client')),
+	server: path.normalize(path.join(__dirname, './src/server'))
+};
 
 var app = express();
 app.configure(function() {
 	app.use(express.logger('dev'));
 	app.use(express.urlencoded());
 	app.use(express.json());
-	app.use(express.static(__dirname + '/webclient'));
+	app.use(express.static(__paths.client));
 });
 
 var port = 8080;
