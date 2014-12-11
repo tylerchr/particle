b9aa25498754b704f5ddfb7c676aa07e3c20faa4
Tyler Christensen <tyler9xp@gmail.com>
2014-12-11 16:22:37 -0700

    Storing the user settings in Mongo now

--- src/server/services/user-settings.js
+++ src/server/services/user-settings.js
@@ -1,32 +1,41 @@
-var Promise = require('bluebird');
+var Promise = require('bluebird'),
+	porqpine = require('porqpine');
 
 module.exports = {
 	getSettings: function(user, service)
 	{
-		if (user == 'tyler9xp@gmail.com' && service == 'lastfm')
-		{
-			return Promise.resolve({
-				username: 'tyler9xp',
-				apiKey: '549cedf90bd7afd5e7883f9f7516ea42'
+		return porqpine.getDb('particle')
+			.then(function(db) {
+				return db.collection('userSettings')
+					.findOneAsync({
+						owner: user,
+						service: service
+					})
+					.then(function(data) {
+						// return just the settings blurb
+						return data.settings;
+					});
 			});
-		}
-		else if (user == 'tyson.decker@gmail.com' && service == 'lastfm')
-		{
-			return Promise.resolve({
-				username: 'tysondecker',
-				apiKey: '549cedf90bd7afd5e7883f9f7516ea42'
+	},
+	setSettings: function(user, service, settings)
+	{
+		return porqpine.getDb('particle')
+			.then(function(db) {
+				return db.collection('userSettings')
+					.updateAsync({
+						owner: user,
+						service: service
+					},
+					{
+						settings: settings
+					},
+					{
+						upsert: true
+					});
+			})
+			.then(function(results) {
+				console.log('Saved %s settings for %s', service, user, results);
+				return results;
 			});
-		}
-		else if (user == 'tyler9xp@gmail.com' && service == 'timeclock')
-		{
-			return Promise.resolve({
-				employeeId: 'TYLERC',
-				workplace: 'Qualtrics'
-			});
-		}
-		else
-		{
-			return Promise.reject('Settings not found');
-		}
 	}
 };
